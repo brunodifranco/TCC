a2039b3a73b3bc8b3793b94f277322dad136a2b2
Bruno Di Franco Albuquerque <66283452+brunodifranco@users.noreply.github.com>
2022-04-12 21:15:36 -0300

    Delete retornos.py

--- retornos.py
+++
@@ -1,7 +0,0 @@
-# Módulo retornos
-def calcular_retornos(X):
-    # Ex de uso: df = calcular_retornos(df)
-    import pandas as pd
-    import numpy as np
-    a = np.log(X).diff().dropna()
-    return a
